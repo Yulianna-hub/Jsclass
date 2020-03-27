98a1c140dacb06fb0cd0359dfefe5bfa9decdf7f
Yuliia Borys <yuliannaboris@gmail.com>
2020-03-27 21:12:20 +0100

    script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+'use ctrict';
+
+let money;
+let income;
+let addExpenses;
+let deposit;
+let mission =('27.03.2020' + ' сдать домашнее задание!');
+let period = ('Обязательно сегодня!');
+
+    console.log(period);
+         alert(mission);
+
+
+
+ 
+
+
+
