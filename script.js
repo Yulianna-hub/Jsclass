d8af4dfccc321141483f8784a995fc0454246c42
Yuliia Borys <yuliannaboris@gmail.com>
2020-03-29 12:34:29 +0200

    script.js

--- script.js
+++ script.js
@@ -1,34 +1,33 @@
-'use ctrict';
+'use strict';
 
 let money = 2000;
-    console.log(typeof money);
+console.log(typeof money);
 
 let income = '500';
-    console.log(typeof income);
+console.log(typeof income);
 
 let addExpenses = 'Интернет, Свет, Топливо';
-    console.log(addExpenses.length);
-    //  - Привести строку addExpenses к нижнему регистру и разбить строку на массив, вывести массив в консоль
-    console.log(addExpenses.toLowerCase());
-    console.log((addExpenses.toLowerCase()).split(','));
+console.log(addExpenses.length);
+//  - Привести строку addExpenses к нижнему регистру и разбить строку на массив, вывести массив в консоль
+console.log(addExpenses.toLowerCase());
+console.log((addExpenses.toLowerCase()).split(','));
 
 
-let deposit = ((2500*60)==735000);
-    console.log(typeof deposit);
+let deposit = ((2500 * 60) == 735000);
+console.log(typeof deposit);
 
 
-let mission = 735000 ;
-    console.log('Цель заработать ' + mission +' евро.')
+let mission = 735000;
+console.log('Цель заработать ' + mission + ' евро.')
 
 let period = 60;
-    console.log('Период равен '+ period +' месяцев.');
+console.log('Период равен ' + period + ' месяцев.');
 
 // - Объявить переменную budgetDay и присвоить дневной бюджет (доход за месяц / 30)
 //- Вывести в консоль budgetDay
 let days = 30;    
-let budgetDay = (money+Number(income))/days;
-    
-    console.log(budgetDay);
+let budgetDay = (money + Number(income)) / days;
+console.log(budgetDay);
 
 
 
